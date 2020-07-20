c6f2e3c36e2f2bb79cad5c52aa83ba79efcfb948
Joshua D <joshuadelapenha@yahoo.com>
2020-07-20 13:14:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,13 +2,16 @@ def using_push(array, string)
   array.push(string)
 end
 
-def using_unshift
+def using_unshift(array, string)
+  array.unshift(string)
 end
 
 def using_pop
+  array.pop
 end 
 
 def pop_with_args
+  
 end 
 
 def using_shift 
